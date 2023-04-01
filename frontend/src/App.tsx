b796003c7a2894e199fad93e3615b075e24fc46d
Heinrich-Codestryke <hb@codestryke.com>
2023-04-01 18:44:45 +0200

    ..

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,36 @@
 import './App.css';
 import ChatBot from './chatbot_vorlage/lib';
 
-const botSendsMessage = () => {
-  return "hallo ich bin dein bot";
-}
-
 const steps=[
   {
     id: '1',
-    message: botSendsMessage,
-    trigger: '2'
+    message: 'What is your name?',
+    trigger: '2',
   },
   {
     id: '2',
     user: true,
-    end: '1',
+    trigger: '3',
+  },
+  {
+    id: '3',
+    options: [
+      { value: 1, label: 'Number 1', trigger: '4' },
+      { value: 2, label: 'Number 2', trigger: '3' },
+      { value: 3, label: 'Number 3', trigger: '3' },
+    ],
+  },
+  {
+    id: '4',
+    component: (
+      <div> This is an example component </div>
+    ),
+    trigger: '5',
+  },
+  {
+    id: '6',
+    message: 'Hi {previousValue}, nice to meet you!',
+    end: true,
   },
 ]
 
@@ -26,8 +42,4 @@ function App() {
   );
 }
 
-
-
-
 export default App;
-
